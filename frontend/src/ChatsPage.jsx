8309e612372989f656f29bc3079fbc3dbf3eb02d
adrianpopescupx <adrianpopescupx@gmail.com>
2023-12-24 23:55:00 +0200

    vercel config

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,16 @@
-const ChatsPage = () => {
-  return <>chats...</>
-}
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
-export default ChatsPage
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId={'294b0652-40b2-4822-a659-7817f5f6544e'}
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
