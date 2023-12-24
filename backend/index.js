ee2c6341479e289588107eb861d4dc2a2aaf741e
adrianpopescupx <adrianpopescupx@gmail.com>
2023-12-24 22:34:01 +0200

    Connected to Cheat Engine

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+const CHAT_ENGINE_PROJECT_ID = "294b0652-40b2-4822-a659-7817f5f6544e";
+const CHAT_ENGINE_PRIVATE_KEY = "e0b326a9-bab8-45dc-be80-e9cfc0f68397";
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  // Stores a user-copy on Chat Engine!
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // User exists inside Chat Engine and I'm fetching it inside this project!
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// vvv On port 3001!
 app.listen(3001);
